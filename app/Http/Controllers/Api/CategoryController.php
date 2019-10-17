28e4afa66088fa309f377e1c29906c7182b6f2a2
Nikita Grebennikov <nikitagreb0204@mail.ru>
2019-10-17 19:32:06 +0300

    finish

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\DB;
+
+class CategoryController extends Controller
+{
+    public function getList()
+    {
+        header('Access-Control-Allow-Origin: ' . env('APP_URL_FRONT'));
+        return DB::connection('mysqlConsole')
+            ->table('category')
+            ->select(DB::raw('category.*, count(news.id) as news_count'))
+            ->leftJoin('news', 'category.id', '=', 'news.category_id')
+            ->groupBy('category.id')
+            ->orderBy('news_count', 'desc')
+            ->get();
+    }
+}
---
+++ app/Models/News.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\News
+ *
+ * @property int $id
+ * @property int $category_id Категория
+ * @property int $source_id Источник
+ * @property string $link Ссылка на новость
+ * @property string $title Заголовок
+ * @property string $description Описание
+ * @property string $image Изображение
+ * @property string $text Текст новости
+ * @property string $status Статус
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Category $category
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereLink($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereSourceId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\News whereUpdatedAt($value)
+ * @mixin \Eloquent
+ * @property-read \App\Models\Source $source
+ */
+class News extends Model
+{
+    /** @var string */
+    public const STATUS_UNPUBLISHED = 'unpublished';
+    /** @var string */
+    public const STATUS_PUBLISHED = 'published';
+
+    protected $connection = 'mysqlConsole';
+
+    protected $table = 'news';
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function source()
+    {
+        return $this->belongsTo(Source::class);
+    }
+}
